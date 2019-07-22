da8e2e5bb3ae20e9b9af5dc9c8569943b81e1297
think-in-universe <mstr.raphael@gmail.com>
2019-07-23 03:42:28 +0800

    implement the open orderbook viewer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import TradeTable from './TradeTable';
+import { Container, Row, Col } from 'react-bootstrap';
 
-function App() {
+
+function App({ location }) {
+  const url = "https://api.steem-engine.com/rpc/contracts";
+  const params = new URLSearchParams(location.search);
+  const token = params.get("token");
+  const order_columns = [{
+      Header: 'Account',
+      accessor: 'account'
+    }, {
+      Header: 'Price (Steem)',
+      accessor: 'price',
+    }, {
+      Header: `${token} Amount`,
+      accessor: 'quantity'
+    }, {
+      Header: "Steem Amount",
+      accessor: 'base_amount'
+    }];
+
+  const history_columns = [{
+      Header: 'Type',
+      accessor: 'type'
+    }, {
+      Header: 'Price (Steem)',
+      accessor: 'price',
+    }, {
+      Header: `${token} Amount`,
+      accessor: 'quantity'
+    }, {
+      Header: "Steem Amount",
+      accessor: 'base_amount'
+    }, {
+      Header: "Date",
+      accessor: "timestamp"
+    }];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>
+        { token } Market
+      </h2>
+      <br />
+
+      <Container>
+        <Row>
+          <Col>
+            <h3>People Buying {token}</h3>
+            <TradeTable url={url} token={token} table="buyBook" index="price" columns={order_columns} descending={true} />
+          </Col>
+          <Col>
+            <h3>People Selling {token}</h3>
+            <TradeTable url={url} token={token} table="sellBook" index="price" columns={order_columns} descending={false} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>{token} Trade History</h3>
+            <TradeTable url={url} token={token} table="tradesHistory" index="timestamp" columns={history_columns} descending={false} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
-export default App;
+function AppRouter() {
+  return (
+    <Router>
+      <Route component={App} />
+    </Router>
+  );
+}
+
+export default AppRouter;
---
+++ src/TradeTable.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import axios from 'axios';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+
+
+export default class TradeTable extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: [],
+      data: [],
+      pages: -1,
+      loading: true
+    }
+  }
+
+  render() {
+    const { url, token, table, columns, index, descending } = this.props;
+
+    return <ReactTable
+      columns={columns}
+      data={this.state.data} // should default to []
+      pageSize={50}
+      loading={this.state.loading}
+      // manual // informs React Table that you'll be handling sorting and pagination server-side
+      onFetchData={(state, instance) => {
+        // show the loading overlay
+        this.setState({loading: true});
+        // fetch your data
+        axios.post(url, {
+          "jsonrpc":"2.0",
+          "id":18,
+          "method":"find",
+          "params": {
+            "contract":"market",
+            "table": table,
+            "query": { "symbol": token },
+            "limit": 500,
+            "offset": 0,
+            "indexes":[{"index":index, "descending": descending}]
+          }
+        })
+          .then((res) => {
+            if (res && res.data && res.data.result) {
+              const result = res.data.result.map(tx => {
+                tx['base_amount'] = Number(tx['price'] * tx['quantity']).toFixed(3);
+                tx['quantity'] = Number(tx['quantity']).toFixed(3);
+                if (tx['timestamp']) {
+                  tx['timestamp'] = new Date(tx['timestamp'] * 1000).toISOString();
+                }
+                return tx;
+              })
+              // update table
+              this.setState({
+                data: result,
+                loading: false
+              })
+            }
+          })
+      }}
+    />
+  }
+}
+
+
+
+
