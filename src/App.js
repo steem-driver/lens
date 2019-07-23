8e28bf606d76262e393f02decb85c5892c7033ca
think-in-universe <mstr.raphael@gmail.com>
2019-07-23 12:34:36 +0800

    refactor lens project source code

--- src/App.js
+++ src/App.js
@@ -5,43 +5,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 
 function App({ location }) {
-  const url = "https://api.steem-engine.com/rpc/contracts";
   const params = new URLSearchParams(location.search);
   const token = params.get("token");
-  const order_columns = [{
-      Header: 'Account',
-      accessor: 'account'
-    }, {
-      Header: 'Price (Steem)',
-      accessor: 'price'
-    }, {
-      Header: `${token} Amount`,
-      accessor: 'quantity'
-    }, {
-      Header: "Steem Amount",
-      accessor: 'base_amount'
-    }, {
-      Header: "Date",
-      accessor: "timestamp"
-    }];
-
-  const history_columns = [{
-      Header: 'Type',
-      accessor: 'type'
-    }, {
-      Header: 'Price (Steem)',
-      accessor: 'price',
-    }, {
-      Header: `${token} Amount`,
-      accessor: 'quantity'
-    }, {
-      Header: "Steem Amount",
-      accessor: 'base_amount'
-    }, {
-      Header: "Date",
-      accessor: "timestamp"
-    }];
-
 
   return (
     <div>
@@ -54,17 +19,17 @@ function App({ location }) {
         <Row>
           <Col>
             <h3>People Buying {token}</h3>
-            <TradeTable url={url} token={token} table="buyBook" index="price" columns={order_columns} descending={true} />
+            <TradeTable token={token} table="buyBook" index="price" descending={true} />
           </Col>
           <Col>
             <h3>People Selling {token}</h3>
-            <TradeTable url={url} token={token} table="sellBook" index="price" columns={order_columns} descending={false} />
+            <TradeTable token={token} table="sellBook" index="price" descending={false} />
           </Col>
         </Row>
         <Row>
           <Col>
             <h3>{token} Trade History</h3>
-            <TradeTable url={url} token={token} table="tradesHistory" index="timestamp" columns={history_columns} descending={false} />
+            <TradeTable token={token} table="tradesHistory" index="timestamp" descending={false} />
           </Col>
         </Row>
       </Container>
--- src/TradeTable.jsx
+++ src/TradeTable.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 
+const url = "https://api.steem-engine.com/rpc/contracts";
 
 export default class TradeTable extends React.Component {
 
@@ -17,10 +18,8 @@ export default class TradeTable extends React.Component {
   }
 
   render() {
-    const { url, token, table, columns, index, descending } = this.props;
-
     return <ReactTable
-      columns={columns}
+      columns={this.columns()}
       data={this.state.data} // should default to []
       pageSize={50}
       loading={this.state.loading}
@@ -28,39 +27,96 @@ export default class TradeTable extends React.Component {
       onFetchData={(state, instance) => {
         // show the loading overlay
         this.setState({loading: true});
-        // fetch your data
-        axios.post(url, {
-          "jsonrpc":"2.0",
-          "id":18,
-          "method":"find",
-          "params": {
-            "contract":"market",
-            "table": table,
-            "query": { "symbol": token },
-            "limit": 500,
-            "offset": 0,
-            "indexes":[{"index":index, "descending": descending}]
-          }
-        })
-          .then((res) => {
-            if (res && res.data && res.data.result) {
-              const result = res.data.result.map(tx => {
-                tx['base_amount'] = Number(tx['price'] * tx['quantity']).toFixed(3);
-                tx['quantity'] = Number(tx['quantity']).toFixed(3);
-                if (tx['timestamp']) {
-                  tx['timestamp'] = new Date(tx['timestamp'] * 1000).toLocaleString();
-                }
-                return tx;
-              })
-              // update table
-              this.setState({
-                data: result,
-                loading: false
-              })
-            }
-          })
+        this.fetchData();
+
       }}
     />
+  }
+
+  columns() {
+    const { token, table } = this.props;
+
+    const order_columns = [{
+      Header: 'Account',
+      accessor: 'account'
+    }, {
+      Header: 'Price (Steem)',
+      accessor: 'price'
+    }, {
+      Header: `${token} Amount`,
+      accessor: 'quantity'
+    }, {
+      Header: "Steem Amount",
+      accessor: 'volume'
+    }, {
+      Header: "Date",
+      accessor: "timestamp"
+    }];
+
+  const history_columns = [{
+      Header: 'Type',
+      accessor: 'type'
+    }, {
+      Header: 'Price (Steem)',
+      accessor: 'price',
+    }, {
+      Header: `${token} Amount`,
+      accessor: 'quantity'
+    }, {
+      Header: "Steem Amount",
+      accessor: 'volume'
+    }, {
+      Header: "Date",
+      accessor: "timestamp"
+    }];
+
+    if (table === "tradesHistory")
+      return history_columns;
+    else
+      return order_columns;
+  }
+
+  fetchData() {
+    const { token, table, index, descending } = this.props;
+    const contract = "market";
+
+    // fetch your data
+    axios.post(url, {
+      "jsonrpc":"2.0",
+      "id":18,
+      "method":"find",
+      "params": {
+        "contract": contract,
+        "table": table,
+        "query": { "symbol": token },
+        "limit": 500,
+        "offset": 0,
+        "indexes":[{"index":index, "descending": descending}]
+      }
+    })
+      .then((res) => {
+        if (table === "tradesHistory") {
+          console.log(res);
+        }
+        if (res && res.data && res.data.result) {
+          const result = res.data.result.map(tx => {
+            if (!tx['volume']) {
+               tx['volume'] = Number(tx['price'] * tx['quantity'])
+            }
+            tx['volume'] = Number(tx['volume']).toFixed(3);
+            tx['quantity'] = Number(tx['quantity']).toFixed(3);
+            if (tx['timestamp']) {
+              tx['timestamp'] = new Date(tx['timestamp'] * 1000).toLocaleString();
+            }
+            return tx;
+          })
+          // update table
+          this.setState({
+            data: result,
+            loading: false
+          })
+        }
+      })
   }
 }
 
