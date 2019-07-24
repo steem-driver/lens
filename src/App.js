6c5ef2ebd82220c398631f209034cb7c99d6bfe2
think-in-universe <mstr.raphael@gmail.com>
2019-07-24 21:36:18 +0800

    add style for table and pages

--- src/App.js
+++ src/App.js
@@ -1,53 +1,80 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 import TradeTable from './TradeTable';
 import TradeHistoryTable from './TradeHistoryTable';
-import { Container, Row, Col } from 'react-bootstrap';
 
 
-function App({ location }) {
+const Header = ({ location }) => {
   const params = new URLSearchParams(location.search);
   const token = params.get("token");
 
   return (
     <div>
-      <h2>
-        { token } Market
-      </h2>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>
+          <img
+            alt=""
+            src="/favicon.ico"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          {' Lens' }
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Link className="nav-link" to={`/open_order?token=${token}`}>Open Orders</Link>
+          <Link className="nav-link" to={`/trade_history?token=${token}`}>Trade History</Link>
+        </Nav>
+      </Navbar>
       <br />
-
-      <Container>
-        { /*<Row>
-          <Col>
-            <h3>People Buying {token}</h3>
-            <TradeTable token={token} table="buyBook" index="price" descending={true} />
-          </Col>
-          <Col>
-            <h3>People Selling {token}</h3>
-            <TradeTable token={token} table="sellBook" index="price" descending={false} />
-          </Col>
-        </Row> */}
-        <Row>
-          <Col>
-            <h3>{token} Trade History</h3>
-            <TradeTable token={token} table="tradesHistory" index="timestamp" descending={false} />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h3>{token} Trade History with Buyers and Sellers</h3>
-            <TradeHistoryTable token={token} index="timestamp" descending={false}/>
-          </Col>
-        </Row>
-      </Container>
     </div>
   );
+}
+
+const OpenOrder = ({ location }) => {
+  const params = new URLSearchParams(location.search);
+  const token = params.get("token");
+
+  return ( <Container>
+    <Row>
+      <Col>
+        <h3>People Buying {token}</h3>
+        <TradeTable token={token} table="buyBook" index="price" descending={true} />
+      </Col>
+      <Col>
+        <h3>People Selling {token}</h3>
+        <TradeTable token={token} table="sellBook" index="price" descending={false} />
+      </Col>
+    </Row>
+  </Container> );
+}
+
+const TradeHistory = ({ location }) => {
+  const params = new URLSearchParams(location.search);
+  const token = params.get("token");
+
+  return ( <Container>
+    <Row>
+      <Col>
+        <h3>{token} Trade History</h3>
+        <TradeTable token={token} table="tradesHistory" index="timestamp" descending={false} />
+      </Col>
+      <Col>
+        <h3>{token} Trade History with Buyers and Sellers</h3>
+        <TradeHistoryTable token={token} index="timestamp" descending={false}/>
+      </Col>
+    </Row>
+  </Container> );
 }
 
 function AppRouter() {
   return (
     <Router>
-      <Route component={App} />
+      <Route component={Header}/>
+      <Route path="/" exact component={OpenOrder} />
+      <Route path="/open_order" component={OpenOrder} />
+      <Route path="/trade_history" component={TradeHistory} />
     </Router>
   );
 }
